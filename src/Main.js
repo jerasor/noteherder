8013754754d6184f08075ed9cbd56a03e2465ae3
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-22 18:23:26 -0400

    Move state from NoteList to main

--- src/Main.js
+++ src/Main.js
@@ -4,24 +4,69 @@ import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-const Main = () => {
-    return (
-        <div 
-            className="Main"
-            style={style}
-        >
-            <Sidebar />
-            <NoteList />
-            <NoteForm />
-        </div>
-    )
+class Main extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            Note: [
+                {
+                    title: 'Kohlrabi welsh',
+                    description: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
+                    active: 'active',
+                },
+                {
+                    title: 'Dandelion cucumber',
+                    description: 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
+                    active: 'notActive',
+                },
+                {
+                    title: 'Prairie turnip',
+                    description: 'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.',
+                    active: 'notActive',
+                },
+            ]
+        }
+    }
+
+    handleNoteClick(item, _ev) {
+        
+        let Note = [...this.state.Note]
+
+        Note.forEach((n) => {
+            if(n===item) {
+                n.active = 'active'
+            } else {
+                n.active = 'notActive'
+            }
+        })
+
+        this.setState({
+            Note: Note
+        })
+
+    }
+    
+    render() {
+        return (
+            <div 
+                className="Main"
+                style={style}
+            >
+                <Sidebar />
+                <NoteList note={this.state.Note} click={this.handleNoteClick.bind(this)} />
+                <NoteForm />
+            </div>
+        )
+    }
 }
 
 const style = {
     display: 'flex',
     height: '100vh',
     alignItems: 'stretch',
-    
+
 }
 
 export default Main
--- src/NoteList.js
+++ src/NoteList.js
@@ -4,48 +4,6 @@ import './NoteList.css'
 
 class NodeList extends React.Component {
 
-    constructor() {
-        super()
-
-        this.state = {
-            Note: [
-                {
-                    title: 'Kohlrabi welsh',
-                    description: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
-                    active: 'active',
-                },
-                {
-                    title: 'Dandelion cucumber',
-                    description: 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.',
-                    active: 'notActive',
-                },
-                {
-                    title: 'Prairie turnip',
-                    description: 'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.',
-                    active: 'notActive',
-                },
-            ]
-        }
-    }
-
-    handleNoteClick(item, _ev) {
-        
-        let Note = [...this.state.Note]
-
-        Note.forEach((n) => {
-            if(n===item) {
-                n.active = 'active'
-            } else {
-                n.active = 'notActive'
-            }
-        })
-
-        this.setState({
-            Note: Note
-        })
-
-    }
-
     render() {
 
         return (
@@ -53,14 +11,14 @@ class NodeList extends React.Component {
             <h3>Notes</h3>
             <ul id="notes">
                 
-                {this.state.Note.map((item)=> {
+                {this.props.note.map((item)=> {
                     return(
                         <a className={item.active}>
                             <li>
                                 <div 
                                     className="note"
                                     onClick={() => (
-                                        this.handleNoteClick(item)
+                                        this.props.click(item)
                                     )}
                                 >
                                 <div className="note-title">
