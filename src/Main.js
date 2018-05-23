f9e28eebed3c7557ea22c0f3e10663a4ca54bf1b
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-22 21:46:10 -0400

    Let NoteForm display active Note data and let it be editable

--- src/Main.js
+++ src/Main.js
@@ -9,6 +9,8 @@ class Main extends React.Component {
     constructor() {
         super()
 
+        
+
         this.state = {
             Note: [
                 {
@@ -26,26 +28,71 @@ class Main extends React.Component {
                     description: 'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.',
                     active: 'notActive',
                 },
-            ]
+            ],
+            activeNoteDescription: 'Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.',
+            activeNoteTitle: 'Kohlrabi welsh',
         }
+
     }
 
     handleNoteClick(item, _ev) {
         
         let Note = [...this.state.Note]
+        let activeDescription = this.state.activeNoteDescription
+        let activeTitle = this.state.activeNoteTitle
 
         Note.forEach((n) => {
             if(n===item) {
                 n.active = 'active'
+                activeDescription = n.description
+                activeTitle = n.title
             } else {
                 n.active = 'notActive'
             }
         })
 
         this.setState({
-            Note: Note
+            Note: Note,
+            activeNoteDescription: activeDescription,
+            activeNoteTitle: activeTitle,
         })
 
+    }
+
+    changeActiveNote(title, description) {
+
+        let note = [...this.state.Note]
+
+        note.forEach((n) => {
+            if(n.active === 'active'){
+                if(description !== undefined){
+                    n.description = description
+                    this.setState({
+                        activeNoteDescription: description
+                    })
+                }
+
+                if(title !== undefined){
+                    n.title = title
+                    this.setState({
+                        activeNoteTitle: title
+                    })
+                }
+            }
+        })
+
+        this.setState({
+            Note: note
+        })
+
+    }
+
+    handleInputChange(ev) {
+        this.changeActiveNote(ev.target.value, undefined)
+    }
+
+    handleTextAreaChange(ev) {
+        this.changeActiveNote(undefined, ev.target.value)
     }
     
     render() {
@@ -56,7 +103,13 @@ class Main extends React.Component {
             >
                 <Sidebar />
                 <NoteList note={this.state.Note} click={this.handleNoteClick.bind(this)} />
-                <NoteForm />
+                <NoteForm 
+                    notes={this.state.Note} 
+                    textChange={this.handleTextAreaChange.bind(this)}
+                    titleChange={this.handleInputChange.bind(this)}
+                    text={this.state.activeNoteDescription}
+                    title={this.state.activeNoteTitle}
+                />
             </div>
         )
     }
--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,27 +2,37 @@ import React from 'react'
 
 import './NoteForm.css'
 
-const NoteForm = () => {
-    return (
-        <div className="NoteForm">
-          <div className="form-actions">
-            <button type="button">
-              <i className="fa fa-trash-o"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-              />
-            </p>
-            
-            <textarea name="body"></textarea>
-          </form>
-        </div>
-    )
+class NoteForm extends React.Component {
+
+    render() {
+
+        return (
+            <div className="NoteForm">
+            <div className="form-actions">
+                <button type="button">
+                <i className="fa fa-trash-o"></i>
+                </button>
+            </div>
+            <form>
+                <p>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title your note"
+                    value={this.props.title}
+                    onChange={this.props.titleChange}
+                />
+                </p>
+                
+                <textarea 
+                    name="body" 
+                    value={this.props.text}
+                    onChange={this.props.textChange}
+                ></textarea>
+            </form>
+            </div>
+        )
+    }
 }
 
 export default NoteForm
