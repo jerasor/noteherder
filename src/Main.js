065a9408acaa79ef0595ee9ec4b6b30eca0ffdc8
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-23 16:45:48 -0400

    Add localStorage save and load to notes array

--- src/Main.js
+++ src/Main.js
@@ -5,83 +5,94 @@ import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
 class Main extends Component {
-  constructor() {
+constructor() {
     super()
+
+    let n = []
+
+    if (localStorage.getItem('notes')) {
+        n = JSON.parse(localStorage.getItem('notes'))
+    }
+
     this.state = {
-      currentNote: this.blankNote(),
-      notes: [],
+        currentNote: this.blankNote(),
+        notes: n,
     }
-  }
+}
 
-  blankNote = () => {
+blankNote = () => {
     return {
-      id: null,
-      title: '',
-      body: '',
+    id: null,
+    title: '',
+    body: '',
     }
-  }
+}
 
-  setCurrentNote = (note) => {
+setCurrentNote = (note) => {
     this.setState({ currentNote: note })
-  }
+}
 
-  resetCurrentNote = () => {
+resetCurrentNote = () => {
     this.setCurrentNote(this.blankNote())
-  }
+}
 
-  saveNote = (note) => {
+saveNote = (note) => {
     const notes = [...this.state.notes]
 
     if (note.id) {
-      // existing note
-      const i = notes.findIndex(currentNote => currentNote.id === note.id)
-      notes[i] = note
+    // existing note
+    const i = notes.findIndex(currentNote => currentNote.id === note.id)
+    notes[i] = note
     } else {
-      // new note
-      note.id = Date.now()
-      notes.push(note)
+    // new note
+    note.id = Date.now()
+    notes.push(note)
     }
 
     this.setState({ notes, currentNote: note })
-  }
+    localStorage.setItem('notes', JSON.stringify(notes))
 
-  deleteNote = (note) => {
+}
+
+deleteNote = (note) => {
     if (note.id !== null) {
+
         const notes = [...this.state.notes]
 
         const i = notes.findIndex(currentNote => currentNote.id === note.id)
         notes.splice(i, 1)
 
         this.setState({ notes, currentNote: this.blankNote() })
+        localStorage.setItem('notes', JSON.stringify(notes))
 
     }
-  }
+}
 
-  render() {
+render() {
     return (
-      <div
+    <div
         className="Main"
         style={style}
-      >
+    >
         <Sidebar resetCurrentNote={this.resetCurrentNote} />
         <NoteList
-          notes={this.state.notes}
-          setCurrentNote={this.setCurrentNote}
+        notes={this.state.notes}
+        setCurrentNote={this.setCurrentNote}
         />
         <NoteForm
-          currentNote={this.state.currentNote}
-          saveNote={this.saveNote}
-          deleteNote={this.deleteNote}
+        currentNote={this.state.currentNote}
+        saveNote={this.saveNote}
+        deleteNote={this.deleteNote}
         />
-      </div>
+    </div>
     )
-  }
+}
 }
 
 const style = {
-  display: 'flex',
-  height: '100vh',
-  alignItems: 'stretch',
+display: 'flex',
+height: '100vh',
+alignItems: 'stretch',
 }
 
 export default Main
