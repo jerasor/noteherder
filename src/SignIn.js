2c010263ecc322e563efd9446ba9601207464861
Jeremy Rasor <jeremyrasor97@gmail.com>
2018-05-24 16:20:36 -0400

    Add google Sign in

--- src/SignIn.js
+++ src/SignIn.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './SignIn.css'
 import quill from './quill.svg'
 import googleLogo from './google.svg'
-import { auth, githubProvider } from './base'
+import { auth, githubProvider, googleProvider } from './base'
 
 const SignIn = () => {
   const authenticate = (provider) => {
@@ -21,11 +21,15 @@ const SignIn = () => {
         <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
         <button
           className="github"
-          onClick={() => authenticate(githubProvider)}>
+          onClick={() => authenticate(githubProvider)}
+        >
           <i className="fab fa-github"></i>
           Sign in with GitHub
         </button>
-        <button className="google">
+        <button 
+          className="google"  
+          onClick={() => authenticate(googleProvider)}
+        >
           <img src={googleLogo} alt="" />
           Sign in with Google
         </button>
